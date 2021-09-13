3d4815e1e3a77d8fe9e9b3f75d7780e4f5f80870
Dharma Raj Thanait <dharmarajthanait25@gmail.com>
2021-09-13 12:14:26 +0545

    multiple other car created and completed task

--- assets/js/car-race.js
+++ assets/js/car-race.js
@@ -6,43 +6,36 @@ let interval;
 
 /*Player Car*/
 const playerCar = {
-  x: Math.floor(Math.random() * (39 - 9)) + 9,
-  y:70,
+  x: Math.floor(Math.random() * (39 - 10)) + 10,
+  y: 70,
   node: null,
-  width:2.604
+  width: 2.604,
 };
 
 /*Other Car*/
-const otherCar={
-    x: Math.floor(Math.random() * (39 - 9)) + 9,
-    y:2,
-    node:null,
-    width:2.604
-}
+const otherCar = {
+  x: Math.floor(Math.random() * (39 - 9)) + 9,
+  y: 2,
+  node: null,
+  width: 2.604,
+};
 window.addEventListener("keydown", moveCar);
 window.addEventListener("keyup", stopCar);
 
 start();
-function start(){
-    displayePlayerCar();
-    displayeOtherCar();
+function start() {
+  displayePlayerCar();
+  displayeOtherCar();
 
-    interval=setInterval(() => {
-        moveRoad();
-        updatePlayerCarPosition();
-        updateOtherCarPosition();
-        removeOtherCar();
-        checkSideCollision();
-        checkCarCollision() ;
-      }, 100);
-
-
+  interval = setInterval(() => {
+    moveRoad();
+    updatePlayerCarPosition();
+    updateOtherCarPosition();
+    removeOtherCar();
+    checkSideCollision();
+    checkCarCollision();
+  }, 50);
 }
-
-
-
-
-
 
 /*Move Road*/
 function moveRoad() {
@@ -54,11 +47,11 @@ function moveRoad() {
 /*Display Player Car*/
 function displayePlayerCar() {
   let playerCarBody = document.createElement("div");
-  playerCarBody.style.width = playerCar.width+"vw";
+  playerCarBody.style.width = playerCar.width + "vw";
   playerCarBody.style.height = "100px";
   playerCarBody.style.position = "absolute";
   playerCarBody.style.left = playerCar.x + "vw";
-  playerCarBody.style.top =playerCar.y+ "vh";
+  playerCarBody.style.top = playerCar.y + "vh";
   playerCarBody.style.backgroundSize = "contain";
   playerCarBody.style.transition = "0.5s linear";
   playerCarBody.style.backgroundImage = "URL('./assets/images/car-green.png')";
@@ -69,21 +62,21 @@ function displayePlayerCar() {
 
 /*Display Other Car*/
 function displayeOtherCar() {
-    let otherCarBody = document.createElement("div");
-   
-    otherCarBody.style.width =otherCar.width+"vw";
-    otherCarBody.style.height = "90px";
-    otherCarBody.style.position = "absolute";
-    otherCarBody.style.left = otherCar.x + "vw";
-    otherCarBody.style.top = otherCar.y +"vh";
-    otherCarBody.style.backgroundSize = "contain";
-    otherCarBody.style.transition = "0.5s linear";
-    otherCarBody.style.transform='rotate(180deg)';
-    otherCarBody.style.backgroundImage = "URL('./assets/images/car-red.png')";
-  
-    let otherCarBodyNode = panel.appendChild(otherCarBody);
-    otherCar.node =otherCarBodyNode;
-  }
+  let otherCarBody = document.createElement("div");
+
+  otherCarBody.style.width = otherCar.width + "vw";
+  otherCarBody.style.height = "90px";
+  otherCarBody.style.position = "absolute";
+  otherCarBody.style.left = otherCar.x + "vw";
+  otherCarBody.style.top = otherCar.y + "vh";
+  otherCarBody.style.backgroundSize = "contain";
+  otherCarBody.style.transition = "0.5s linear";
+  otherCarBody.style.transform = "rotate(180deg)";
+  otherCarBody.style.backgroundImage = "URL('./assets/images/car-red.png')";
+
+  let otherCarBodyNode = panel.appendChild(otherCarBody);
+  otherCar.node = otherCarBodyNode;
+}
 
 /*Check Road Side Collision */
 function checkSideCollision() {
@@ -94,16 +87,16 @@ function checkSideCollision() {
 
 /*Check Collision between cars */
 function checkCarCollision() {
-    let x = playerCar.x - otherCar.x;
-    let y = playerCar.y - otherCar.y;
-    let distance= Math.sqrt( x*x + y*y );
+  let x = playerCar.x - otherCar.x;
+  let y = playerCar.y - otherCar.y;
+  let distance = Math.sqrt(x * x + y * y);
 
-    let collisionDistance=(playerCar.width+otherCar.width)/2;
-    if (distance<=collisionDistance) {
-      gameOver();
-    }
+  let collisionDistance = (playerCar.width + otherCar.width) / 2;
+  if (distance <= collisionDistance) {
+    gameOver();
   }
-  
+}
+
 /*Update Player Car Position */
 function updatePlayerCarPosition() {
   playerCar.x = playerCar.x + playerCarDirection.x;
@@ -111,20 +104,19 @@ function updatePlayerCarPosition() {
 }
 
 /*Update Other Car Position */
-function updateOtherCarPosition(){
-    otherCar.y= otherCar.y+otherCarDirection.x;
-    otherCar.node.style.top= otherCar.y+"vh";
+function updateOtherCarPosition() {
+  otherCar.y = otherCar.y + otherCarDirection.x;
+  otherCar.node.style.top = otherCar.y + "vh";
 }
 
 /*Remove other car */
-function removeOtherCar(){
-
-    if(otherCar.y>=85){
-        otherCar.node.style.display='none';
-    }
-
+function removeOtherCar() {
+  if (otherCar.y >= 95) {
+    otherCar.y=2;
+    otherCar.x=Math.floor(Math.random() * (39 - 9)) + 9;
+    displayeOtherCar();
+  }
 }
-  
 
 /*Key down Event*/
 function moveCar(e) {
@@ -150,27 +142,25 @@ function stopCar(e) {
   }
 }
 
-
 /*Game Over Function */
-function gameOver(){
-    playerCar.node.style.display='none';
-    otherCar.node.style.display='none';
-    window.clearInterval(interval);
-    let gameOverBody = document.createElement("div");
+function gameOver() {
+  playerCar.node.style.display = "none";
+  otherCar.node.style.display = "none";
+  window.clearInterval(interval);
+  let gameOverBody = document.createElement("div");
 
-    gameOverBody.style.position = "absolute";
-    gameOverBody.style.left = "10vw";
-    gameOverBody.style.top ="10vh";
-    gameOverBody.style.transition = "0.5s linear";
-    gameOverBody.innerHTML="<h1>Game Over!!!</h1> <button id='tryagain-btn'>Try Again</button>";
-    gameOverBody.style.color='red';
-    gameOverBody.style.fontSize='3rem';
-    panel.appendChild(gameOverBody);
-    let tryAgainBtn=document.getElementById("tryagain-btn");
+  gameOverBody.style.position = "absolute";
+  gameOverBody.style.left = "10vw";
+  gameOverBody.style.top = "10vh";
+  gameOverBody.style.transition = "0.5s linear";
+  gameOverBody.innerHTML =
+    "<h1>Game Over!!!</h1> <button id='tryagain-btn'>Try Again</button>";
+  gameOverBody.style.color = "red";
+  gameOverBody.style.fontSize = "3rem";
+  panel.appendChild(gameOverBody);
+  let tryAgainBtn = document.getElementById("tryagain-btn");
 
-    tryAgainBtn.addEventListener("click",()=>{
-        location.reload();
-    });
-
-
+  tryAgainBtn.addEventListener("click", () => {
+    location.reload();
+  });
 }
